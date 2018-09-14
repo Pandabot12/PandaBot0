ddf77af64397f31279504b6f2df35d640606b5f4
Pandabot12 <43148855+Pandabot12@users.noreply.github.com>
2018-09-14 02:21:52 +0200

    Tets

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
-var tmi = require ('tmi.js')
-
+var tmi = require ('tmi.js');
 var options = {
   options: {
     debug: true
@@ -21,42 +20,43 @@ client.connect();
 /// on start \\\
 
 client.on('connected', function(address, port) {
-  console.log("[PandaBot] Bot was started!")
+  console.log("[PandaBot] Bot was started!");
 });
 
 client.on('connected', function(address, port) {
-  client.say("BeL1kePanda_Bot", "Hello! I joined to this channel")
+  client.say("BeL1kePanda_Bot", "Hello! I joined to this channel");
 });
 
 ///comm\\\
-var sw = 0
-var dw = 0
-var sqw = 0
-var sk = []
-var dk = []
-var sqk = []
-var t222
+var sw = 0;
+var dw = 0;
+var sqw = 0;
+var sk = [];
+var dk = [];
+var sqk = [];
+var t222;
 function  myWins(){
-  var swins = "Solo: "+sw+" Wins ("+sk+" Kills)"
-  var dwins = "Duo: "+dw+" Wins ("+dk+" Kills)"
-  var sqwins = "Squad: "+sqw+" Wins ("+sqk+" Kills)"
+  var swins = "Solo: "+sw+" Wins ("+sk+" Kills)";
+  var dwins = "Duo: "+dw+" Wins ("+dk+" Kills)";
+  var sqwins = "Squad: "+sqw+" Wins ("+sqk+" Kills)";
   var nowins = "Unfortunately, no wins today";
+  var sp = ", ";
   if(sw > 0 && dw > 0 && sqw > 0) {
-    t222 = swins+sp+dwins+sp+sqwins}
+    t222 = swins+sp+dwins+sp+sqwins;}
     else if(sw > 0 && dw > 0){
-      t222 = swins+sp+dwins}
+      t222 = swins+sp+dwins;}
       else if(sw > 0 && sqw > 0){
-        t222 = swins+sp+sqwins}
+        t222 = swins+sp+sqwins;}
         else if(dw > 0 && sqw > 0){
-          t222 = dwins+sp+sqwins}
+          t222 = dwins+sp+sqwins;}
           else if(sw > 0){
-            t222 = swins}
+            t222 = swins;}
             else if(dw > 0){
-              t222 = dwins}
+              t222 = dwins;}
               else if(sqw > 0){
-                t222 = sqwins}
+                t222 = sqwins;}
                 else {
-                  t222 = nowins}
+                  t222 = nowins;}
 }
 
 //Solo
@@ -115,13 +115,13 @@ client.on('chat', function(channel, user, message, self){
 //Reset
 client.on('chat', function(channel, user, message, self){
   if(message === "!reset"){
-    sw = 0
-    dw = 0
-    sqw = 0
-    sk = []
-    dk = []
-    sqk = []
-    t222
+    sw = 0;
+    dw = 0;
+    sqw = 0;
+    sk = [];
+    dk = [];
+    sqk = [];
+    t222 =
     myWins();
     client.say("BeL1kePanda_Bot",t222);}
   });
@@ -130,7 +130,7 @@ client.on('chat', function(channel, user, message, self){
 client.on('chat', function(channel, user, message, self){
   if(message === "!wins"){
     myWins();
-    client.say("BeL1kePanda_Bot", t222)}
+    client.say("BeL1kePanda_Bot", t222);}
   });
 
   ///console.log (user.user-type);
